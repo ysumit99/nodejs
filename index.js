ab3096492ebfa6296cd6e15da6c1d33300d93e57
Sumit Yadav <sumityadav@Sumits-MacBook-Pro.local>
2018-06-07 02:05:57 +0530

    start a server example

---
+++ index.js
@@ -0,0 +1,21 @@
+/*  
+* Primary file for the API
+*
+*
+*/
+
+//Dependencies
+var http = require('http');
+
+//the server should respond to all requests with a string
+
+	
+	var server = http.createServer(function(req,res){
+		res.end("Hello World\n");
+	});
+
+//start the server and have it listen on port 3000
+
+server.listen(3000,function(){
+	console.log("The server is listening on port 3000 now...");
+});
