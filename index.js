f18023b8745cd5576c4d2e009ba99ff0c57516dc
Sumit Yadav <sumityadav@Sumits-MacBook-Pro.local>
2018-06-09 14:20:37 +0530

    Http request parsing

--- index.js
+++ index.js
@@ -6,12 +6,36 @@
 
 //Dependencies
 var http = require('http');
+var url = require('url');
 
-//the server should respond to all requests with a string
 
-	
-	var server = http.createServer(function(req,res){
+		//the server should respond to all requests with a string
+
+		var server = http.createServer(function(req,res){
+
+		//Get the Url and parse it
+
+		var parsedUrl = url.parse(req.url,true);
+
+		//Get the path
+
+		var path = parsedUrl.pathname;
+
+		var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+		//Get the HTTP Method
+
+		var method = req.method.toLowerCase();
+
+		//Send the response
+
 		res.end("Hello World\n");
+
+		//Log the request path
+		console.log('Request received on path: '+trimmedPath+ ' with method '+method);
+
+
+
 	});
 
 //start the server and have it listen on port 3000
